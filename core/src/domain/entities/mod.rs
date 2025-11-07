153d9732315eec4f828be10c91b5707737916cb8
Nathael Bonnal <64804778+NathaelB@users.noreply.github.com>
2025-11-07 18:26:28 +0100

    feat: Introduce ID generation and domain ports (#8)
    * feat: Introduce ID generation and domain ports
    
    * feat: add notification entity and serde derives
    
    * feat: add Config struct

--- core/src/domain/entities/mod.rs
+++ core/src/domain/entities/mod.rs
@@ -1,14 +1,48 @@
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 pub mod events;
+pub mod notification;
 pub mod preference;
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct UserId(pub Uuid);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct ChannelId(pub Uuid);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-#[allow(unused)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct NotificationId(pub Uuid);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct PreferenceId(pub Uuid);
+
+impl std::fmt::Display for PreferenceId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<Uuid> for PreferenceId {
+    fn from(uuid: Uuid) -> Self {
+        PreferenceId(uuid)
+    }
+}
+
+impl From<Uuid> for NotificationId {
+    fn from(uuid: Uuid) -> Self {
+        NotificationId(uuid)
+    }
+}
+
+impl From<Uuid> for ChannelId {
+    fn from(uuid: Uuid) -> Self {
+        ChannelId(uuid)
+    }
+}
+
+impl From<Uuid> for UserId {
+    fn from(uuid: Uuid) -> Self {
+        UserId(uuid)
+    }
+}
--- core/src/domain/mod.rs
+++ core/src/domain/mod.rs
@@ -1,3 +1,18 @@
+use beep_server::config::AuthConfig;
+use thiserror::Error;
+
+use crate::domain::entities::PreferenceId;
+
 pub mod entities;
 pub mod ports;
 pub mod services;
+
+#[derive(Debug, Error)]
+pub enum CoreError {
+    #[error("Preference with id {id} not found")]
+    PreferenceNotFound { id: PreferenceId },
+}
+
+pub struct Config {
+    pub auth: AuthConfig,
+}
